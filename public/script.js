ded055445e545d2227cac7de4e9f099dc369c355
Valentina <gavalentina365@gmail.com>
2025-07-27 20:44:43 -0500

    feat:agregamos nuevos campos para el formulario en la función summit

--- public/script.js
+++ public/script.js
@@ -4,11 +4,17 @@ const lista = document.getElementById('listaUsuarios');
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
   const nombre = document.getElementById('nombre').value;
+  const password = document.getElementById('password').value;
+  const documento = document.getElementById('document').value;
+  const city = document.getElementById('city').value;
+  const age = document.getElementById('age').value;
+  const gender = document.getElementById('gender').value;
+  const id = Math.random()
 
   await fetch('/api/usuarios', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ nombre })
+    body: JSON.stringify({ nombre,password,id,documento,city,age,gender })
   });
 
   cargarUsuarios();
